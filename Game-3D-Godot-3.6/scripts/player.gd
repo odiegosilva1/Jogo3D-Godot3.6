2d360bf43b6ee8fe751f97e66d059cef3d0702e5
odiegosilva1 <odiegosilvadev@gmail.com>
2025-06-01 02:06:10 -0300

    change: player.gd

--- Game-3D-Godot-3.6/scripts/player.gd
+++ Game-3D-Godot-3.6/scripts/player.gd
@@ -1,52 +1,122 @@
 extends KinematicBody
 
-var speed = 10
-var accel = 20
-var gravity = 9.8
-var jump = 5
+# Configurações de movimento
+export var speed = 10.0
+export var acceleration = 5.0
+export var deceleration = 10.0
+export var gravity = -24.8
+export var jump_force = 12.0
+export var mouse_sensitivity = 0.3
 
-var mousesense = 0.05
-var dire = Vector3()
-var vel = Vector3()
-var fall = Vector3()
+# Configurações da câmera em terceira pessoa
+export var camera_distance = 8.0
+export var min_camera_angle = -30.0
+export var max_camera_angle = 90.0
+export var camera_collision_offset = 0.5
 
-onready var head = $Head
+# Variáveis de movimento
+var velocity = Vector3()
+var snap_vector = Vector3()
+
+# Referências
+var camera_pivot = null
+var camera = null
+var spring_arm = null
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Esconde e prende o cursor no início
+	# Configura os nós da câmera
+	setup_camera_nodes()
+	
+	# Configuração do mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func setup_camera_nodes():
+	# Cria ou obtém o CameraPivot
+	if not has_node("CameraPivot"):
+		camera_pivot = Spatial.new()
+		camera_pivot.name = "CameraPivot"
+		add_child(camera_pivot)
+		camera_pivot.owner = get_tree().edited_scene_root
+	else:
+		camera_pivot = $CameraPivot
+	
+	# Cria ou obtém o SpringArm
+	if not camera_pivot.has_node("SpringArm"):
+		spring_arm = SpringArm.new()
+		spring_arm.name = "SpringArm"
+		spring_arm.spring_length = camera_distance
+		spring_arm.collision_mask = 1
+		camera_pivot.add_child(spring_arm)
+		spring_arm.owner = get_tree().edited_scene_root
+	else:
+		spring_arm = $CameraPivot/SpringArm
+	
+	# Cria ou obtém a Camera
+	if not spring_arm.has_node("Camera"):
+		camera = Camera.new()
+		camera.name = "Camera"
+		spring_arm.add_child(camera)
+		camera.owner = get_tree().edited_scene_root
+	else:
+		camera = $CameraPivot/SpringArm/Camera
+	
+	# Posiciona a câmera corretamente
+	camera.transform.origin = Vector3.ZERO
+	spring_arm.spring_length = camera_distance
 
 func _input(event):
-	if Input.is_action_just_pressed("shot"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Esconde e prende o cursor
-
-	if Input.is_action_just_pressed("pause"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # Mostra o cursor
-	
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(deg2rad(-event.relative.x * mousesense))
-		head.rotate_x(deg2rad(-event.relative.y * mousesense))
-		head.rotation.x = clamp(head.rotation.x, deg2rad(-30), deg2rad(15))
+		# Rotação horizontal (player)
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		
+		# Rotação vertical (câmera) com limites
+		if camera_pivot:
+			var new_rotation = camera_pivot.rotation_degrees.x - (event.relative.y * mouse_sensitivity)
+			camera_pivot.rotation_degrees.x = clamp(new_rotation, min_camera_angle, max_camera_angle)
+	
+	if event.is_action_pressed("ui_cancel"):
+		toggle_mouse_capture()
+
+func toggle_mouse_capture():
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	dire = Vector3()
+	handle_movement(delta)
 
-	# Gravidade
-	if not is_on_floor():
-		fall.y -= gravity * delta
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		fall.y = jump
-
-	# Movimento
+func handle_movement(delta):
+	var input_dir = Vector3.ZERO
+	
 	if Input.is_action_pressed("w"):
-		dire -= transform.basis.z
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("s"):
+		input_dir += global_transform.basis.z
 	if Input.is_action_pressed("a"):
-		dire -= transform.basis.x
-	elif Input.is_action_pressed("s"):
-		dire += transform.basis.z
-	elif Input.is_action_pressed("d"):
-		dire += transform.basis.x
-
-	dire = dire.normalized()
-	vel = vel.linear_interpolate(dire * speed, accel * delta) 
-	vel = move_and_slide(vel, Vector3.UP)
-	move_and_slide(fall, Vector3.UP)
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("d"):
+		input_dir += global_transform.basis.x
+	
+	input_dir = input_dir.normalized()
+	
+	var target_velocity = input_dir * speed
+	var current_accel = acceleration if input_dir.length() > 0 else deceleration
+	
+	velocity.x = lerp(velocity.x, target_velocity.x, current_accel * delta)
+	velocity.z = lerp(velocity.z, target_velocity.z, current_accel * delta)
+	
+	velocity.y += gravity * delta
+	
+	if is_on_floor():
+		snap_vector = -get_floor_normal()
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = jump_force
+			snap_vector = Vector3.ZERO
+	else:
+		snap_vector = Vector3.DOWN
+	
+	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector3.UP, true, 4, deg2rad(45))
+	
+	if is_on_floor() and velocity.y < 0:
+		velocity.y = 0
